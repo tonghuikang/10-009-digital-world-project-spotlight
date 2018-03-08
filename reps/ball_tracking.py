c8ec5f3bc398c77539324fb0460273ff25b1eae2
Tong Hui Kang <tonghuikang@outlook.com>
2018-03-09 01:04:01 +0800

    x

--- reps/ball_tracking.py
+++ reps/ball_tracking.py
@@ -9,6 +9,10 @@ import argparse
 import imutils
 import cv2
 
+cv2.setUseOptimized(True)
+
+from camera import PiVideoStream
+
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
 ap.add_argument("-v", "--video",
@@ -24,19 +28,23 @@ greenLower = (29, 86, 6)
 greenUpper = (64, 255, 255)
 pts = deque(maxlen=args["buffer"])
 
-# if a video path was not supplied, grab the reference
-# to the webcam
-if not args.get("video", False):
-	camera = cv2.VideoCapture(0)
+## if a video path was not supplied, grab the reference
+## to the webcam
+#if not args.get("video", False):
+#    camera = cv2.VideoCapture(0)
+#
+## otherwise, grab a reference to the video file
+#else:
+#    camera = cv2.VideoCapture(args["video"])
 
-# otherwise, grab a reference to the video file
-else:
-	camera = cv2.VideoCapture(args["video"])
+camera = PiVideoStream().start()
+
+time.sleep(1.0)
 
 # keep looping
 while True:
 	# grab the current frame
-	(grabbed, frame) = camera.read()
+	frame = camera.read()
 
 	# if we are viewing a video and we did not grab a frame,
 	# then we have reached the end of the video
