e8c9c67e3a924eb8baf520609b5e6c44c235a30b
Tong Hui Kang <tonghuikang@outlook.com>
2018-03-22 13:03:16 +0800

    rotate

--- boundaryfollower_thymio/smbrainsim3.py
+++ boundaryfollower_thymio/smbrainsim3.py
@@ -5,9 +5,9 @@ from libdw import sm
 from boxworld import thymio_world
 
 class MySMClass(sm.SM):
-    start_state = 0.1, 0.1
+    start_state = 0.1, 0.0, False  # speed, rot, wall
     def get_next_values(self, state, inp):
-        fv_, rv_ = state
+        fv_, rv_, wall = state
         # These two lines is to stop the robot
         # by pressing the backward button.
         # This only works when using the real robot.
@@ -19,13 +19,20 @@ class MySMClass(sm.SM):
         #ground = inp.prox_ground.reflected
         #ground = inp.prox_ground.ambiant
         
-        #ground = inp.prox_ground.delta
-        #left = ground[0]
-        #right = ground[1]
-        #print(left,right)
-        rv_ *= 0.9
-        next_state = fv_, rv_
-        return next_state, io.Action(fv=fv_, rv=rv_)
+        ground = inp.prox_ground.delta
+        left = ground[0]
+        right = ground[1]
+        print(left,right)
+        
+        if wall:
+            if left > 200 and right > 200: # both white
+                return next_state, io.Action(fv=0.03, rv=1)
+            if left < 200 and right < 200: # both black
+                return next_state, io.Action(fv=0.03, rv=-1)
+            else:
+                return next_state, io.Action(fv=0.1, rv=rv_)
+
+        return next_state, io.Action(fv=0.1, rv=rv_)
     
     #########################################
     # Don't modify the code below.
