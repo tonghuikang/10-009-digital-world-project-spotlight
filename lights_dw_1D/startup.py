2d3e4d2830ec11827c3f80eb44a4710fada4503e
sohdesune <38250886+sohdesune@users.noreply.github.com>
2018-04-17 23:23:44 +0800

    back to old test but in startup

--- lights_dw_1D/startup.py
+++ lights_dw_1D/startup.py
@@ -1,1 +1,28 @@
 print("hello world")
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+pins = [17, 22, 13, 21]
+
+GPIO.cleanup()
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pins, GPIO.OUT)
+
+def activate_led(pin_list, duty_list):
+    pwm = [GPIO.PWM(item, 50) for item in pin_list]
+    for i in range(len(pwm)):
+        pwm[i].start(duty_list[i] * 100)
+    print("LEDs activated accordingly: {}".format(duty_list))
+    sleep(1.5)
+
+activate_led(pins, [1, 0.9, 0.5, 1])
+activate_led(pins, [0.9, 0.8, 0.8, 1])
+activate_led(pins, [0.8, 0, 0.4, 0.8])
+activate_led(pins, [0.7, 0.6, 0.7, 0])
+activate_led(pins, [0.6, 0.5, 0, 1])
+activate_led(pins, [0, 0.4, 1, 0.9])
+activate_led(pins, [0.4, 0.3, 0.9, 1])
+
+GPIO.cleanup()
+print("GPIO cleanup completed")
