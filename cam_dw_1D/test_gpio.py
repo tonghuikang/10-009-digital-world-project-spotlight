1b4ba3233f43806847e0bf76685e7f34de62d351
sohdesune <38250886+sohdesune@users.noreply.github.com>
2018-04-17 16:52:51 +0800

    test 4 leds now

--- cam_dw_1D/test_gpio.py
+++ cam_dw_1D/test_gpio.py
@@ -1,7 +1,7 @@
 from time import sleep
 import RPi.GPIO as GPIO
 
-pins = [21, 5, 13]
+pins = [17, 22, 13, 21]
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins, GPIO.OUT)
@@ -15,13 +15,13 @@ def activate_led(pins, duty_list):
     print("LEDs activated accordingly: {}".format(duty_list))
     sleep(1)
 
-activate_led(pins, [1, 0.9, 0.5])
-activate_led(pins, [0.9, 0.8, 0.8])
-activate_led(pins, [0.8, 0, 0.4])
-activate_led(pins, [0.7, 0.6, 0.7])
-activate_led(pins, [0.6, 0.5, 0])
-activate_led(pins, [0, 0.4, 1])
-activate_led(pins, [0.4, 0.3, 0.9])
+activate_led(pins, [1, 0.9, 0.5, 1])
+activate_led(pins, [0.9, 0.8, 0.8, 1])
+activate_led(pins, [0.8, 0, 0.4, 0.8])
+activate_led(pins, [0.7, 0.6, 0.7, 0])
+activate_led(pins, [0.6, 0.5, 0, 1])
+activate_led(pins, [0, 0.4, 1, 0.9])
+activate_led(pins, [0.4, 0.3, 0.9, 1])
 
 GPIO.cleanup()
 print("GPIO cleanup completed")
