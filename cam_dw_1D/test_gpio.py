8ef8dc196b35c9edfb37308d685e32126372a450
sohdesune <38250886+sohdesune@users.noreply.github.com>
2018-04-17 22:26:51 +0800

    better test, and to try pwm.stop()

--- cam_dw_1D/test_gpio.py
+++ cam_dw_1D/test_gpio.py
@@ -6,22 +6,22 @@ pins = [17, 22, 13, 21]
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(pins, GPIO.OUT)
 
-#activate LEDs according to duty cycle assigned
-def activate_led(pins, duty_list):
-    print("Activating LEDs")
-    pwm = [GPIO.PWM(item, 50) for item in pins]
+def activate_led(pin_list, duty_list):
+    pwm = [GPIO.PWM(item, 50) for item in pin_list]
     for i in range(len(pwm)):
         pwm[i].start(duty_list[i] * 100)
     print("LEDs activated accordingly: {}".format(duty_list))
-    sleep(1)
+    sleep(1.5)
 
-activate_led(pins, [1, 0.9, 0.5, 1])
-activate_led(pins, [0.9, 0.8, 0.8, 1])
-activate_led(pins, [0.8, 0, 0.4, 0.8])
-activate_led(pins, [0.7, 0.6, 0.7, 0])
-activate_led(pins, [0.6, 0.5, 0, 1])
-activate_led(pins, [0, 0.4, 1, 0.9])
-activate_led(pins, [0.4, 0.3, 0.9, 1])
+dut = [(x+1)/10 for x in range(10)]
+duty_test = []
+for i in range(10):
+    duty_test.append([dut[i], dut[-(i+1)], dut[i], dut[-(i+1)]])
 
+for i in range(10):
+    activate_led(pins, duty_test[i])
+
+pwm = [GPIO.PWM(item, 50) for item in pins]
+pwm.stop()
 GPIO.cleanup()
 print("GPIO cleanup completed")
